0a81283dd1819c343de6ad3bf7cae9cb8356e562
sharoncy <s.c.yates@medisin.uio.no>
2023-08-30 12:46:05 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "ebrains_logo.png"
+html_logo = "General_logo_Landscape_Color.svg"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
